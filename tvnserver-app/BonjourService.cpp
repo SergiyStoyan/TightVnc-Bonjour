29ed5af25ec7494f2154aa1e20871dae6a3dbfa5
cliversoft <sergey.stoyan@gmail.com>
2016-11-04 03:59:12 +0200

    .

--- tvnserver-app/BonjourService.cpp
+++ tvnserver-app/BonjourService.cpp
@@ -13,13 +13,7 @@
 #include "win-system/SCMClient.h"
 #include "win-system/Environment.h"
 
-BonjourService::BonjourService(WinServiceEvents *winServiceEvents,
-                       NewConnectionEvents *newConnectionEvents)
-: Service(ServiceNames::SERVICE_NAME),// m_tvnServer(0),
-//  m_winServiceEvents(winServiceEvents),
-//  m_newConnectionEvents(newConnectionEvents),
-  m_logServer(LogNames::LOG_PIPE_PUBLIC_NAME),
-  m_clientLogger(LogNames::LOG_PIPE_PUBLIC_NAME, LogNames::SERVER_LOG_FILE_STUB_NAME)
+BonjourService::BonjourService()
 {
 }
 
@@ -27,78 +21,10 @@ BonjourService::~BonjourService()
 {
 }
 
-void BonjourService::onStart()
+void BonjourService::Start()
 {
-	try {
-		m_winServiceEvents->enable();
-		// FIXME: Use real logger instead of zero.
-		//m_tvnServer = new TvnServer(true, m_newConnectionEvents, this, &m_clientLogger);
-		//m_tvnServer->addListener(this);
-		m_winServiceEvents->onSuccServiceStart();
-	}
-	catch (Exception &e) {
-		m_winServiceEvents->onFailedServiceStart(&StringStorage(e.getMessage()));
-	}
 }
 
-void BonjourService::install()
+void BonjourService::Stop()
 {
-  StringStorage binPath;
-
-  //TvnService::getBinPath(&binPath);
-
-  SCMClient scManager;
-
-  scManager.installService(ServiceNames::SERVICE_NAME,
-                           ServiceNames::SERVICE_NAME_TO_DISPLAY,
-                           binPath.getString(), _T(""));
 }
-
-void BonjourService::remove()
-{
-  SCMClient scManager;
-
-  scManager.removeService(ServiceNames::SERVICE_NAME);
-}
-
-void BonjourService::reinstall()
-{
-  try {
-    remove();
-  } catch (...) { }
-
-  BonjourService::install();
-}
-
-void BonjourService::start(bool waitCompletion)
-{
-  SCMClient scManager;
-
-  scManager.startService(ServiceNames::SERVICE_NAME, waitCompletion);
-}
-
-void BonjourService::stop(bool waitCompletion)
-{
-  SCMClient scManager;
-
-  scManager.stopService(ServiceNames::SERVICE_NAME, waitCompletion);
-}
-
-//bool BonjourService::getBinPath(StringStorage *binPath)
-//{
-//  StringStorage pathToServiceBinary;
-//
-//  // Get executable folder first.
-//  if (!Environment::getCurrentModulePath(&pathToServiceBinary)) {
-//    return false;
-//  }
-//
-//  // Create formatted binary path.
-//  binPath->format(_T("\"%s\" %s"),
-//                  pathToServiceBinary.getString(),
-//                  SERVICE_COMMAND_LINE_KEY);
-//
-//  return true;
-//}
-
-
--- tvnserver-app/BonjourService.h
+++ tvnserver-app/BonjourService.h
@@ -8,73 +8,14 @@
 #ifndef _BONJOUR_SERVICE_H_
 #define _BONJOUR_SERVICE_H_
 
-#include "log-server/LogServer.h"
-#include "log-server/ClientLogger.h"
-#include "win-system/Service.h"
-
-#include "thread/Thread.h"
-#include "WinServiceEvents.h"
-#include "NewConnectionEvents.h"
-
-class BonjourService : public Service//,
-                   //public TvnServerListener,
-                   //private LogInitListener
+class BonjourService 
 {
-  /**
-   * Creates object.
-   */
-	BonjourService(WinServiceEvents *winServiceEvents,
-             NewConnectionEvents *newConnectionEvents);
-  /**
-   * Deletes object.
-   */
-  virtual ~BonjourService();
+public:
+	BonjourService();
+   ~BonjourService();
 
-  /**
-   * Installs tvnserver service.
-   * @throws SystemException on fail.
-   */
-  static void install() throw(SystemException);
-  /**
-   * Stops and removes tvnserver service.
-   * @throws SystemException when failed to remove service.
-   */
-  static void remove() throw(SystemException);
-  /**
-   * Reinstalls tvnserver service (combite call of remove and install methods).
-   * @remark ignores if remove call throws exception.
-   * @throws SystemException when fail to install service.
-   */
-  static void reinstall() throw(SystemException);
-  /**
-   * Starts tvnserver service.
-   * @param waitCompletion if true, wait until the status becomes
-   *   SERVICE_RUNNING.
-   * @throws SystemException on fail.
-   */
-  static void start(bool waitCompletion = false) throw(SystemException);
-  /**
-   * Stopps tvnserver service.
-   * @param waitCompletion if true, wait until the status becomes
-   *   SERVICE_STOPPED.
-   * @throws SystemException on fail.
-   */
-  static void stop(bool waitCompletion = false) throw(SystemException);
-
-protected:
-
-	/**
-	* Inherited from superclass.
-	* Starts tvnserver execution.
-	* @throws SystemException when failed to start.
-	*/
-	virtual void onStart() throw(SystemException);
-
-  LogServer m_logServer;
-  ClientLogger m_clientLogger;
-
-  WinServiceEvents *m_winServiceEvents;
-  NewConnectionEvents *m_newConnectionEvents;
+   static void Start();
+   static void Stop();
 };
 
 #endif
